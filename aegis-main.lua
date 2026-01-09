a4e93b1e32e2d53496eb37252446667e76075283
qthompson2 <q.thompson088@gmail.com>
2026-01-09 08:01:24 -0800

    Sets up rough project structure.

---
+++ aegis-main.lua
@@ -0,0 +1,17 @@
+local function hash(str)
+
+end
+
+local function encrypt(str, username)
+
+end
+
+local function decrypt(str, username)
+
+end
+
+return {
+	hash = hash,
+	encrypt = encrypt,
+	decrypt = decrypt
+}
